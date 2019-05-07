36edb5d2d1b08e8f64bbc77d30083f8d610b7efe
SaadyBennani <sbennani23@gmail.com>
2019-05-06 19:12:50 -0700

    added login

--- Django_Todos/urls.py
+++ Django_Todos/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('todos.urls')),
-    path('', TemplateView.as_view(template_name='test.html'), name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
